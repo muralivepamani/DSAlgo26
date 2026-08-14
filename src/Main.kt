057427c4cc7605c71fec1d8252283720aef35b70
murali.vepamani <vepamanimurali495@gmail.com>
2026-08-14 17:38:19 +0530

    Monotonic Stack / Queue

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,23 @@
+import common.printLinkedList
 import fastslowpointers.ListNode
-import fastslowpointers.`Middle of the Linked List`
+import reversallinkedlist.`Reverse Linked List`
+import reversallinkedlist.`Reverse Linked List II`
+import reversallinkedlist.`Reverse Nodes in k-Group`
+import stackqueue.`Daily Temperatures`
+import stackqueue.`Largest Rectangle in Histogram`
+import stackqueue.`Next Greater Element I`
+
+// Helper function to print linked list nodes in "1 -> 2 -> 3" format
+
 
 fun main() {
-    // 1. Create nodes
-    val node0 = ListNode(1)
-    val node1 = ListNode(2)
-    val node2 = ListNode(3)
-    val node3 = ListNode(4)
-    val node4 = ListNode(5)
 
-    // 2. Link them sequentially: 1 -> 2 -> 3 -> 4 -> 5
-    node0.next = node1
-    node1.next = node2
-    node2.next = node3
-    node3.next = node4
+        val solution = `Largest Rectangle in Histogram`()
+        val input=intArrayOf(2,1,5,6,2,3)
+        println(input.joinToString())
+        val result = solution.largestRectangleArea(input)
 
-    val head = node0
+        // Prints the result array: [1, 1, 4, 2, 1, 1, 0, 0]
+        println(result)
 
-    // 3. Test middleNode function
-    val solution = `Middle of the Linked List`()
-    val result = solution.middleNode(head)
-
-    // 4. Print the value of the middle node
-    println("Middle Node Value: ${result?.`val`}") // Output: 3
 }
